733ea3b4cb7f40460566fb0da4066b21970ffd73
yeosiowvic <victoryeo1974@gmail.com>
2023-11-01 14:54:55 +0800

    update deploy script

--- SmartContract/scripts/deploy.ts
+++ SmartContract/scripts/deploy.ts
@@ -1,12 +1,20 @@
 import hre from "hardhat";
 
 async function main() {
-  const factory = await hre.ethers.getContractFactory("HelloWorld");
-  const contract = await factory.deploy();
+  /*const factory1 = await hre.ethers.getContractFactory("HelloWorld");
+  const contract1 = await factory1.deploy();
 
-  await contract.deployed();
+  await contract1.deployed();
+  console.log("Hello contract deployed to:", contract1.address);*/
 
-  console.log("contract deployed to:", contract.address);
+  const signer: any[] = await hre.ethers.getSigners()
+  console.log(signer[0].address)
+
+  const factory2 = await hre.ethers.getContractFactory("SpaceBunnies");
+  const contract2 = await factory2.deploy("T_NAME", "T_SYMBOL", signer[0].address);
+
+  await contract2.deployed();
+  console.log("NFT contract deployed to:", contract2.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
