ccd1ea8c929b60538f254ae0c259023e774244a1
yeosiowvic <victoryeo1974@gmail.com>
2023-11-02 09:17:07 +0800

    print out nft token id that is minted

--- SmartContract/scripts/mint.ts
+++ SmartContract/scripts/mint.ts
@@ -1,19 +1,20 @@
 import hre from "hardhat";
 
 async function main() {
-  const collectionAddress = "0x234855669984493948A9898765";
+  // on local testnet
+  const collectionAddress = "0xe78A0F7E598Cc8b0Bb87894B0F60dD2a88d6a8Ab";
+
   const signer: any[] = await hre.ethers.getSigners()
   const signerAddress = await signer[0].getAddress()
-  console.log(signerAddress)
-  const nftContract = await hre.ethers.getContractAt("T_NAME", collectionAddress, signerAddress);
-  const tokenData = {
-    "name": "NFT_NAME",
-    "image": "IMAGE_URL",
-  }
+  console.log("signerAddress", signerAddress)
+  const nftContract = await hre.ethers.getContractAt("SpaceBunnies", collectionAddress);
+
   const tx = await nftContract.safeMint(signerAddress);
-  await tx.wait()
-  
-  console.log("A new NFT has been minted to:", signerAddress);
+  const resu = await tx.wait()
+  //console.log(resu)
+  const nftInx = Number(resu.events[0].args[2])
+  console.log("A new NFT index ", nftInx, "has been minted" );
+  console.log("tokenURI:", await nftContract.tokenURI(nftInx))
 }
 
 // We recommend this pattern to be able to use async/await everywhere
