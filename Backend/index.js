98c4efa0c991347101b0efea83836c217302db09
yeosiowvic <victoryeo1974@gmail.com>
2023-11-03 19:57:40 +0800

    add fireblocks create vault endpoint

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+const { FireblocksSDK } = require('fireblocks-sdk');
+const fs = require('fs');
+const path = require('path');
+const { inspect } = require("util");
 
 require("dotenv").config();
 
 const app = express();
 const port = 8090;
+
+console.log(process.env.FIREBLOCKS_API_PRIVATE_KEY_PATH)
+const privateKey = fs.readFileSync(
+  path.resolve(process.env.FIREBLOCKS_API_PRIVATE_KEY_PATH ?? ''), "utf8");
+const apiKey = process.env.FIREBLOCKS_API_KEY ?? '';
+// Choose the right api url for your workspace type 
+const baseUrl = "https://sandbox-api.fireblocks.io";
+const fireblocks = new FireblocksSDK(privateKey, apiKey, baseUrl);
 
 app.use(
   cors({
@@ -26,8 +38,20 @@ const server = app.listen(port, () => {
 
 const apiPath = {
   helloworld: "/api/helloworld",
+  createVault: "/api/createVault",
 };
 
 app.get(apiPath.helloworld, (req, res) => {
   return res.status(200).set("Content-Type", "application/json").send("helloworld\n");
 });
+
+app.post(apiPath.createVault, async (req, res) => {
+  // Print vaults before creation
+  let vaultAccounts = await fireblocks.getVaultAccountsWithPageInfo({});
+  console.log(inspect(vaultAccounts, false, null, true));
+
+  return res
+    .status(200)
+    .set("Content-Type", "application/json")
+    .send(vaultAccounts.accounts[0]);
+})
