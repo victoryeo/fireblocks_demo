638c81f54464ccc21ce293a9fb78eb9c3217d0b2
yeosiowvic <victoryeo1974@gmail.com>
2023-12-19 14:48:25 +0800

    update txReceipt log

--- Backend/index.js
+++ Backend/index.js
@@ -112,10 +112,11 @@ app.post(apiPath.mintNFT, async (req, res) => {
   try {
   const txHash = await nftContract.methods.safeMint(myAddr[0]).send(
     {from: myAddr[0], gas: 1000000});
-  console.log(txHash)
+  console.log("txHash", txHash)
   
   const txReceipt = await web3.eth.getTransactionReceipt(txHash.transactionHash);
-  console.log(txReceipt)
+  console.log("txReceipt", txReceipt)
+  console.log(txReceipt.logs)
   console.log(txReceipt.logs[0].topics[3])
   
   const nftInx = Number(txReceipt.logs[0].topics[3]);
